cf3712dc5df2834a2927bfca1f80ff1892066ecb
fabianreyescordero <93954201+fabianreyescordero@users.noreply.github.com>
2022-07-17 11:22:11 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    while (input.temperature() > 25) {
+        basic.showNumber(input.temperature())
+        basic.showIcon(IconNames.Happy)
+    }
+    while (input.temperature() < 23) {
+        basic.showNumber(input.temperature())
+        basic.showIcon(IconNames.Sad)
+    }
+})
